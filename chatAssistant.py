343cb3b7cd2ffc7b0d79fb0909c3b807f8e4e300
Vinicius Tom Vidal <viniciustom@hotmail.com>
2024-03-14 16:32:41 -0300

    Background

--- chatAssistant.py
+++ chatAssistant.py
@@ -89,14 +89,14 @@ if api_key:
 #uploaded_file = st.sidebar.file_uploader("Envie um arquivo", key="file_uploader")
 uploaded_file = download_file("https://tecnologia2.chleba.net/_ftp/chatgpt/BotasVentoPedidos.csv")
 # Botão para iniciar o chat
-if st.sidebar.button("Enviar pedidos"):
+if st.sidebar.button("Enviar pedidos em CSV"):
     if uploaded_file:
         # Converter XLSX para PDF
         pdf_output_path = "converted_file.md"
-        convert_xlsx_to_json(uploaded_file, pdf_output_path)
+        #convert_xlsx_to_json(uploaded_file, pdf_output_path)
 
         # Enviar o arquivo convertido
-        additional_file_id = upload_to_openai(pdf_output_path)
+        additional_file_id = upload_to_openai(uploaded_file)
 
         st.session_state.file_id_list.append(additional_file_id)
         st.sidebar.write(f"ID do arquivo: {additional_file_id}")
